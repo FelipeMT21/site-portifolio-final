2bcee6677918e07ea75257c91833aa3e2cee5857
Felipe Silva <joaofelipems21@gmail.com>
2024-11-01 11:56:47 -0300

    Add Footer Component and fixed resolution mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { SkillsComponent } from "./skills/skills.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, AboutComponent, ProjectsComponent, SkillsComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, AboutComponent, ProjectsComponent, SkillsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
