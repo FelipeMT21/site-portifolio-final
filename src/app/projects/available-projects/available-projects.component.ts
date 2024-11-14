49cc0edda4312b27e398e3efff49a6395ad3c456
Felipe Silva <joaofelipems21@gmail.com>
2024-11-14 15:03:47 -0300

    Improvement of Execptions

--- src/app/projects/available-projects/available-projects.component.ts
+++ src/app/projects/available-projects/available-projects.component.ts
@@ -1,7 +1,7 @@
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { Project } from '../../models/project.model';
 import { HttpClient } from '@angular/common/http';
-import { delay } from 'rxjs';
+import { catchError, delay, throwError } from 'rxjs';
 import { ProjectsComponent } from '../projects.component';
 
 @Component({
@@ -14,16 +14,27 @@ import { ProjectsComponent } from '../projects.component';
 export class AvailableProject implements OnInit {
   projects = signal<Project[] | undefined>(undefined);
   isFetching = signal<boolean>(false);
+  error = signal<string | null>(null);
   private httpClient = inject(HttpClient);
   destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     this.isFetching.set(true);
     const subscription = this.httpClient.get<Project[]>('http://localhost:8080/projects').pipe(
-      delay(3000)
+      delay(3000),
+      catchError((error) => {
+        console.log('Error do backend:', error);
+        const errorMessage = error.error?.message || 'Algo deu errado na busca dos projetos. Por favor tente mais tarde.';
+        this.error.set(errorMessage);
+        return throwError(() => new Error(errorMessage))
+      })
     ).subscribe({
       next: (resData) => {
         this.projects.set(resData);
+        this.error.set(null);
+      },
+      error: (error: Error) => {
+        console.log('Erro processado:', error.message);
       },
       complete: () => {
         this.isFetching.set(false);
