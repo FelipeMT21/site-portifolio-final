98594c13e8c7d8ba7e5e1c4818bccb34cf834690
Felipe Silva <joaofelipems21@gmail.com>
2024-12-01 11:48:22 -0300

    Add Added ec2 elastic link

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    apiUrl: (window as any)['API_URL'] || 'https://projects-springboot3-jpa.onrender.com/projects' // URL para produção
+    apiUrl: (window as any)['API_URL'] || 'http://52.4.89.243:8080/projects' // URL para produção
 }
